6cc2a4d5cf654dd7c752eeb7ee53924bd165ca36
Samee Alam <sameealam50@gmail.com>
2026-04-06 13:15:11 +0530

    updated

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ import adminAuth from "./src/routes/adminAuth.Routes.js";
 import userAuth from "./src/routes/userAuth.Routes.js";
 import records from "./src/routes/record.Routes.js";
 import analytics from "./src/routes/analytics.Routes.js";
+import cors from "cors";
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(cookieParser());
+
+app.use(
+    cors({
+        origin : "http://localhost:5173",
+        credentials : true,
+        methods : ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+        allowHeaders : ["Content-Type","Authorization"],
+    }),
+);
 
 app.use("/api/v1/auth",adminAuth);
 app.use("/api/v1/user",userAuth);
